66eb06ffdc03c094306cf6fd0df45005345fffca
Denis_Bednakov <50529176+bednakovdenis@users.noreply.github.com>
2021-02-24 12:15:19 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 apt update 
-apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
-curl -O https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tar.xz
-tar -xf Python-3.8.2.tar.xz
-cd Python-3.8.2
-./configure --enable-optimizations
-make -j 4
-make altinstall
-mkdir ~/my_app && cd ~/my_app
-python3.8 -m venv my_app_venv
-source my_app_venv/bin/activate
-cd ..
-pip install poetry
-pip install --upgrade pip
+apt install python3.9
+apt-get install python3-pip
+pip3 install poetry
 poetry install
 poetry run python -m app
